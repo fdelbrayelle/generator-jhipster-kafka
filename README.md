74097a0d5a2fb55d41386a05fbf8a59cd123bc0d
François Delbrayelle <fdelbrayelle@gmail.com>
2019-12-24 16:24:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# generator-jhipster-kafka
+
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+
+> JHipster module, A JHipster module to generate Apache Kafka consumers and producers.
+
+# Introduction
+
+This is a [JHipster](https://www.jhipster.tech/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](https://www.jhipster.tech/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://www.jhipster.tech/installation/)
+
+# Installation
+
+## With NPM
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-kafka
+```
+
+To update this module:
+
+```bash
+npm update -g generator-jhipster-kafka
+```
+
+## With Yarn
+
+To install this module:
+
+```bash
+yarn global add generator-jhipster-kafka
+```
+
+To update this module:
+
+```bash
+yarn global upgrade generator-jhipster-kafka
+```
+
+# Usage
+
+# License
+
+Apache-2.0 © [François Delbrayelle](https://fdelbrayelle.github.io/)
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-kafka.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-kafka
+[travis-image]: https://travis-ci.org/fdelbrayelle/generator-jhipster-kafka.svg?branch=master
+[travis-url]: https://travis-ci.org/fdelbrayelle/generator-jhipster-kafka
+[daviddm-image]: https://david-dm.org/fdelbrayelle/generator-jhipster-kafka.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/fdelbrayelle/generator-jhipster-kafka
---
+++ test/app.spec.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const fse = require('fs-extra');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+describe('JHipster generator kafka', () => {
+    describe('Test with Maven and AngularX', () => {
+        beforeEach(done => {
+            helpers
+                .run(path.join(__dirname, '../generators/app'))
+                .inTmpDir(dir => {
+                    fse.copySync(path.join(__dirname, '../test/templates/maven-angularX'), dir);
+                })
+                .withOptions({
+                    testmode: true
+                })
+                .withPrompts({
+                    message: 'simple message to say hello'
+                })
+                .on('end', done);
+        });
+
+        it('generate dummy.txt file', () => {
+            assert.file(['dummy-maven.txt', 'dummy-angularX.txt']);
+        });
+    });
+
+    describe('Test with Gradle and React', () => {
+        beforeEach(done => {
+            helpers
+                .run(path.join(__dirname, '../generators/app'))
+                .inTmpDir(dir => {
+                    fse.copySync(path.join(__dirname, '../test/templates/gradle-react'), dir);
+                })
+                .withOptions({
+                    testmode: true
+                })
+                .withPrompts({
+                    message: 'simple message to say hello'
+                })
+                .on('end', done);
+        });
+
+        it('generate dummy.txt file', () => {
+            assert.file(['dummy-gradle.txt', 'dummy-react.txt']);
+        });
+    });
+});
